5f8f47c89e9cbeda4f200740f3083f7155b92c71
BIKRAM GORAI <bikram77620@gmail.com>
2026-02-27 20:09:38 +0530

    database connected testing

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 import app from "./src/app.js";
+import { connectDB } from "./src/config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+// Connect Database
+connectDB();
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at: http://localhost:${PORT}`);
+  console.log(`
+=================================
+🚀 Server running
+🌍 URL: http://localhost:${PORT}
+=================================
+`);
 });
